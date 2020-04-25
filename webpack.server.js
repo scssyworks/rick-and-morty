51c7ceb946263f9dac6cc6fb52009cb5bb39bd88
Sachin Singh <ssingh259@sapient.com>
2020-04-26 00:42:36 +0530

    Added production config

--- webpack.server.js
+++ webpack.server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
-const webpack = require('webpack');
 
 const config = {
     // Set current mode to development
